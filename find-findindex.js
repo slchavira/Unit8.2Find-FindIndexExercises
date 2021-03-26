06c269e2c8c32b136b3b3bab0d62e75ad7d76528
Stoner Chavira <slchavira@gmail.com>
2021-03-26 03:43:43 -0500

    final commit. exercises complete.

--- find-findindex.js
+++ find-findindex.js
@@ -12,7 +12,11 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(val){
+    return val.username === username;
+  });
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +31,15 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  let idx = usersArray.findIndex(function(val){   // This will assign index value of user if found.
+    return val.username === username;
+  });
+  if (idx === -1) {
+    return;
+  } else {
+    // let removedUser = usersArray.splice(idx,1);
+    // return removedUser[0];
+    return usersArray.splice(idx,1)[0];
+  }
+}
